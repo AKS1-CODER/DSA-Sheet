27acbc40705729e53e94a6779eb8702f184d0088
AKS1-CODER <kumaranmol9484@gmail.com>
2026-01-14 23:59:27 +0530

    functions of cpp

---
+++ step 1 learn the basics/step 1.1 things to know/functions.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void funtopartition(int n){
+    int i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+
+    }
+
+}
+int main() {
+    int n;
+    cin>>n;
+    funtopartition(n);
+    return 0;
+}
