0d75bf8c108d55381b9193d18c31a2450a615bbd
AKS1-CODER <kumaranmol9484@gmail.com>
2026-01-15 23:07:20 +0530

    functions more clearance

--- step 1 learn the basics/step 1.1 things to know/functions.cpp
+++ step 1 learn the basics/step 1.1 things to know/functions.cpp
@@ -1,17 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-void funtopartition(int n){
+void funtopartition(int& n){
     int i=1;
     while(i<=n){
         cout<<i<<endl;
         i++;
+    }        
 
     }
+ void test(int &x, int y){
+    x+=5;
+    y*=2;
+    cout<<x<<y;
+}   
 
-}
+
 int main() {
     int n;
     cin>>n;
     funtopartition(n);
+
+    int a = 3, b = 4;
+    test(a,b);
+    cout<<a<<b;
+
+
+
+
+
     return 0;
 }
+
+
+    
