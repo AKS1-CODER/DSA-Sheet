5b0c827d873d1eb3151d731266f4b5c6c3cd31b2
AKS1-CODER <kumaranmol9484@gmail.com>
2026-03-10 21:45:18 +0530

    new pattern

--- step 1 learn the basics/step 1.1 things to know/pattern10.cpp
+++ step 1 learn the basics/step 1.1 things to know/pattern10.cpp
@@ -1,3 +1,34 @@
 /*
-
+*
+**
+***
+****
+*****
+****
+***
+**
+*
 */
+#include<bits/stdc++.h>
+using namespace std;
+void print1(int n){
+    for (int i=0; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
+void print2(int n){
+    for (int i=0; i<=n; i++){
+        for (int j=1; j<=n-i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    print1(5);
+    print2(4);
+}
